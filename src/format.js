047880ed387a8cff6dd6fff4596e3a16c9570281
Aaron Sikes <aaron@sikes.io>
2017-11-14 09:52:33 -0500

    Put the formatting all together into a message formatter

--- src/format.js
+++ src/format.js
@@ -1,4 +1,7 @@
-module.exports.color = function(text, color, {bold}={}) {
+const util = require('util');
+const humanize = require('ms');
+
+function ansiColor(text, color, {bold}={}) {
   if (color < 8) {
     color = '3' + color;
   } else {
@@ -10,11 +13,11 @@ module.exports.color = function(text, color, {bold}={}) {
   return `\u001b[${color}m${text}\u001b[0m`;
 };
 
-module.exports.prefixLines = function(prefix, text) {
+function prefixLines(prefix, text) {
   return text.split('\n').map(line => prefix + line).join('\n');
 };
 
-module.exports.runFormatters = function(args, formatters) {
+function runFormatters(args, formatters) {
   let result = args.slice();
   let str = result[0];
   let index = 0;
@@ -28,4 +31,63 @@ module.exports.runFormatters = function(args, formatters) {
     }
   });
   return result;
+};
+
+function formatMessage(args, channel, options={}) {
+  let {
+    color,
+    diff,
+    formatters,
+    interactive,
+    timestamp
+  } = options;
+
+  formatters = formatters || {};
+
+  // run custom formatters
+  args = runFormatters(args, formatters);
+  // run builtin formatters
+  let text = util.format(...args);
+
+  if (color) {
+    channel = ansiColor(channel, color, {bold:true});
+  }
+
+  // Always prefixed with the channel name.
+  // If there's a timestamp, it goes first
+  let prefix;
+  if (timestamp != null) {
+    let ts = timestamp.toISOString();
+    prefix = `${ts} ${channel} `;
+  } else {
+    prefix = channel + ' ';
+  }
+
+  // +120ms or something goes at the end, same
+  // color as the channel name
+  let suffix = '';
+  if (diff != null) {
+    suffix = '+' + humanize(diff);
+    if (color) {
+      suffix = ansiColor(suffix, color);
+    }
+    suffix = ' ' + suffix
+  }
+
+  // when going to the terminal it's nice for indentation's
+  // sake if all lines of the message get the channel name,
+  // but that's not great for automated logs.
+
+  if (interactive) {
+    return prefixLines('  '+prefix, text) + suffix;
+  } else {
+    return prefix + text + suffix;
+  }
+};
+
+module.exports = {
+  color: ansiColor,
+  prefixLines,
+  runFormatters,
+  formatMessage
 };
--- test/spec/formatting-test.js
+++ test/spec/formatting-test.js
@@ -1,25 +1,25 @@
 const assert = require('assert');
 const format = require('@cardstack/logger/src/format');
 
-describe('Formatter', function() {
-  describe('#color', function() {
-    it('supports named colors', function() {
-      let result = format.color('hello', 4);
-      assert.equal(result, '\u001b[34mhello\u001b[0m');
+describe("Formatter", function() {
+  describe("#color", function() {
+    it("supports named colors", function() {
+      let result = format.color("hello", 4);
+      assert.equal(result, "\u001b[34mhello\u001b[0m");
     });
 
-    it('supports 8-bit colors', function() {
-      let result = format.color('hello', 111);
-      assert.equal(result, '\u001b[38;5;111mhello\u001b[0m');
+    it("supports 8-bit colors", function() {
+      let result = format.color("hello", 111);
+      assert.equal(result, "\u001b[38;5;111mhello\u001b[0m");
     });
 
-    it('supports bolding the text', function() {
-      let result = format.color('hello', 4, { bold: true });
-      assert.equal(result, '\u001b[1;34mhello\u001b[0m');
+    it("supports bolding the text", function() {
+      let result = format.color("hello", 4, { bold: true });
+      assert.equal(result, "\u001b[1;34mhello\u001b[0m");
     });
   });
 
-  describe('#prefixLines', function() {
+  describe("#prefixLines", function() {
     it("Adds the prefix before every line", function() {
       let input = `
 hello
@@ -30,17 +30,73 @@ world
 - world
 `.trim();
 
-      assert.equal(format.prefixLines('- ', input), expected);
+      assert.equal(format.prefixLines("- ", input), expected);
     });
   });
 
   describe("#runFormatters", function() {
     it("will run formatters found in the formatter dictionary", function() {
       let result = format.runFormatters(["%d and %h", 50, {}], {
-        h: () => 'hello'
+        h: () => "hello"
       });
 
-      assert.deepEqual(result, ["%d and %s", 50, 'hello']);
+      assert.deepEqual(result, ["%d and %s", 50, "hello"]);
+    });
+  });
+
+  describe("#formatMessage", function() {
+    it("prefixes lines with the channel name when interactive", function() {
+      let result = format.formatMessage(["hi\nhello"], "test", {
+        interactive: true
+      });
+      assert.equal(result, "  test hi\n  test hello");
+    });
+
+    it("prefixes once with the channel name when not interactive", function() {
+      let result = format.formatMessage(["hi\nhello"], "test", {
+        interactive: false
+      });
+      assert.equal(result, "test hi\nhello");
+    });
+
+    it("runs util formatters", function() {
+      let result = format.formatMessage(["%s", "hi"], "test", {
+        interactive: true
+      });
+      assert.equal(result, "  test hi");
+    });
+
+    it("runs custom formatters", function() {
+      let result = format.formatMessage(["%h", 5], "test", {
+        formatters: {
+          h: () => "hi"
+        }
+      });
+      assert.equal(result, "test hi");
+    });
+
+    it("appends a diff", function() {
+      let result = format.formatMessage(["hi"], "test", {
+        diff: 700
+      });
+      assert.equal(result, "test hi +700ms");
+    });
+
+    it("prepends a timestamp", function() {
+      let result = format.formatMessage(["hi"], "test", {
+        timestamp: new Date('2012-12-12')
+      });
+      assert.equal(result, "2012-12-12T00:00:00.000Z test hi");
+    });
+
+    it("colors the channel name and millisecond diff", function() {
+      let result = format.formatMessage(["hi"], "test", {
+        color: 4,
+        diff: 120
+      });
+      let boldColorChannel = format.color("test", 4, {bold:true});
+      let colorDiff = format.color("+120ms", 4);
+      assert.equal(result, `${boldColorChannel} hi ${colorDiff}`);
     });
   });
 });
