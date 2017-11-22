ff5db0b8b2a1340d11092a71de0f40cba7e28638
Aaron Sikes <aaron@sikes.io>
2017-11-22 17:44:47 -0500

    Add proper license and authoring information
    Originally this was set to be a fork of tj/debug, but pretty much the
    whole module was written from scratch. Instead, we're just including the
    original license with the code that was derived directly.

--- src/color.js
+++ src/color.js
@@ -1,5 +1,30 @@
-// named terminal colors
-let colors = [ 6, 2, 3, 4, 5, 1 ];
+/*
+(The MIT License)
+
+Copyright (c) 2014 TJ Holowaychuk <tj@vision-media.ca>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
+and associated documentation files (the 'Software'), to deal in the Software without restriction, 
+including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial 
+portions of the Software.
+
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT 
+LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+// Taken from
+// https://github.com/visionmedia/debug/blob/285dfe10a5c06d4a86176b54bef2d7591eedaf40/src/node.js#L23
+// and
+// https://github.com/visionmedia/debug/blob/285dfe10a5c06d4a86176b54bef2d7591eedaf40/src/common.js#L46
+
+let colors = [ 6, 2, 3, 4, 5, 1 ];   // named terminal colors
 try {
   var supportsColor = require('supports-color');
   if (supportsColor && (supportsColor.stderr || supportsColor).level >= 2) {
