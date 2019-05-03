c817c9371c55d8911a1b52265902ab391f2b3795
Edward Faulkner <ef@alum.mit.edu>
2019-05-03 17:02:55 -0400

    port patterns

--- src/patterns.ts
+++ src/patterns.ts
@@ -1,13 +1,13 @@
-module.exports.compile = function compile(str) {
+export function compile(str: string) {
   return new RegExp(str.split('*').map(escapeRegExp).join('.*'), 'i');
 };
 
 // https://codereview.stackexchange.com/a/153702
-function escapeRegExp(string){
+function escapeRegExp(string: string){
   return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
 }
 
-module.exports.findMatch = function findMatch(rules, value, defaultVal) {
+export function findMatch<T>(rules: ([RegExp, T])[], value: string, defaultVal: T) {
   for (let i = rules.length - 1; i >= 0; i--) {
     let rule = rules[i];
     if (rule[0].test(value)) {
