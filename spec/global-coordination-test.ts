2568c0de80f59ec88b7bdd61ef2fd9e911013c57
Edward Faulkner <ef@alum.mit.edu>
2019-05-07 22:38:17 -0400

    port global coordination tests

--- spec/global-coordination-test.ts
+++ spec/global-coordination-test.ts
@@ -1,18 +1,20 @@
-const assert = require('assert');
+import assert from 'assert';
+
+/* eslint-disable @typescript-eslint/no-var-requires, @typescript-eslint/no-require-imports, @typescript-eslint/camelcase */
 
 describe('Global coordination between multiple instances of the module', function() {
   beforeEach(function() {
     delete require.cache[require.resolve('..')];
-    delete global.__global_cardstack_logger;
+    delete (global as any).__global_cardstack_logger;
   });
 
   it("registers itself globally if it's the first loaded", function() {
     let loggerModule = require('..');
-    assert.strictEqual(loggerModule, global.__global_cardstack_logger);
+    assert.strictEqual(loggerModule, (global as any).__global_cardstack_logger);
   });
 
   it("uses an already-registered version", function() {
-    global.__global_cardstack_logger = {
+    (global as any).__global_cardstack_logger = {
       iAmFirst: true
     };
 
